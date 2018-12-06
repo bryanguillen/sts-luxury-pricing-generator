0e01eb1030b56270ba3298282df0b94abc61d280
bryg217 <bryan.guillen217@gmail.com>
2018-12-06 02:10:24 -0500

    updated get endpoint

--- index.js
+++ index.js
@@ -69,7 +69,7 @@ app.post('/csvUploads', upload.single(CSV_FILE_INPUT_FIELD_NAME), function(req, 
  */
 app.get('/tmp/:fileName', (req, res) => {
   const fileName = req.params.fileName;
-  const file = path.join(__dirname + '\\tmp\\' + fileName);
+  const file = path.join(__dirname + '/tmp/' + fileName);
   console.log('!!!!!!!!Exists!!!!!!!!', fs.existsSync())
   res.status(200).sendFile(file, (err) => {
     if (err) {
