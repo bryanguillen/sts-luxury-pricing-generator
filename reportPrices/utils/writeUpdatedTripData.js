0a01cb86dc5ae695757728c466722c5f206f59e5
bryg217 <bryan.guillen217@gmail.com>
2018-12-05 23:44:15 -0500

    updated function to return new file name-- new file name is timestamp

--- reportPrices/utils/writeUpdatedTripData.js
+++ reportPrices/utils/writeUpdatedTripData.js
@@ -6,8 +6,9 @@
  * @author Bryan Guillen
  */
 
- const Json2CsvParser = require('json2csv').Parser;
- const fs = require('fs');
+const Json2CsvParser = require('json2csv').Parser;
+const fs = require('fs');
+const generateTimeStamp = require('./generateTimeStamp');
 
 function writeUpdatedTripData(file, updatedTripsData) {
   return new Promise((resolve, reject) => {
@@ -18,29 +19,20 @@ function writeUpdatedTripData(file, updatedTripsData) {
          .then((trips) => {
            const json2csvParser = new Json2CsvParser({ trips, doubleQuote: '' });
            const csv = json2csvParser.parse(trips);
+           const newFile = 'csvUploads\\' + generateTimeStamp() + '.csv';
 
-           if (typeof file === STRING && file.match(/csv$/) === null) {
-             file += '.csv';
-             deleteCurrentFile = true; // HACK!!! Delete file with non-csv extension -- the CSV is later deleted, check index.js
-           }
-
-           fs.writeFile(file, csv, (error) => {
+           fs.writeFile(newFile, csv, (error) => {
              if (error) {
                throw error;
              }
 
-             if (deleteCurrentFile) {
-               // HACK FOR DELETING ORIGINAL UPLOADED FILE!!!!
-               let uploadedFileWithoutExt = file;
-               fs.unlink(uploadedFileWithoutExt.replace(/\.csv$/, ''), (err) => {
-                 if (err) {
-                   throw new Error(err);
-                 }
-                 resolve();
-               });
-             } else {
-              resolve();
-             }
+             fs.unlink(file, (err) => {
+               if (err) {
+                 throw new Error(err);
+               }
+
+               resolve(newFile);
+             });
            });
          })
          .catch((error) => {
