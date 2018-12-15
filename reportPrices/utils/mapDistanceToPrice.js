0d03d5f7030155ff9d4272e60b3d03f41b6f55dd
bryg217 <bryan.guillen217@gmail.com>
2018-12-15 00:50:06 -0500

    Updated the formula used within the else statement in order to subtract 8 miles instead of 9 and multiply by 200 instead of 210

--- reportPrices/utils/mapDistanceToPrice.js
+++ reportPrices/utils/mapDistanceToPrice.js
@@ -1,6 +1,9 @@
 /**
  * @description Function that maps a distance, in miles and passed
- * in as a Number, to a price.
+ * in as a Number (integer not float), to a price.
+ * The algorithm follows the rules laid out in the code, which come from
+ * company requirements. Note: the formula within the else case
+ * essentially means $2.00/mile after 8 miles, plus the base $17.  
  *
  * @author Bryan Guillen
  */
@@ -9,11 +12,11 @@ function mapDistanceToPrice(miles) {
   let price = '';
 
   if (miles < 6) {
-    price = 15;
+    price = 15.00;
   } else if (miles < 9) {
     price = 17.00;
   } else {
-    price = (17 + ((210 * (miles - 9)) / 100));
+    price = (17 + ((200 * (miles - 8)) / 100));
   }
 
   return price.toFixed(2);
